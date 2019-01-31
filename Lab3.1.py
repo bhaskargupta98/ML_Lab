dad67c4b99b5eb15bd512dd8844fb26ce5080a4e
Bhaskar Gupta <guptab3@gmail.com>
2019-01-31 11:20:41 +0530

    Created Lab3.1
    https://medium.com/@thomascountz/19-line-line-by-line-python-perceptron-b6f113b161f3 
    Use above link for reference

---
+++ Lab3.1.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Created on Mon Jan 14 11:50:41 2019
+
+
+
+@author: Student
+
+"""
+
+#Implementing 3 input AND gate perceptron that gives the output of three binary inputs
+import numpy as np
+
+class Perceptron(object):
+    
+    def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        self.weights = np.zeros(no_of_inputs + 1)
+        
+    def predict(self, inputs):
+        summation = np.dot(inputs, self.weights[1:]) +  self.weights[0]
+        if summation > 0:
+            activation = 1
+        else:
+            activation = 0
+        return activation
+    def train(self, training_inputs, labels):
+        for _ in range(self.threshold):
+            for inputs, label in zip(training_inputs, labels):
+                prediction = self.predict(inputs)
+                self.weights[1:] += self.learning_rate *(label-prediction) * inputs
+                self.weights[0] += self.learning_rate *(label-prediction)
+# Training the model
+                
+training_inp = []
+training_inp.append(np.array([1,1,1]))
+training_inp.append(np.array([0,0,0]))
+training_inp.append(np.array([0,0,1]))
+training_inp.append(np.array([0,1,0]))
+training_inp.append(np.array([0,1,1]))
+training_inp.append(np.array([1,0,0]))
+training_inp.append(np.array([1,0,1]))
+
+#training_inp.append(np.array[1,1,0])
+labels = np.array([1,0,0,0,0,0,0])
+perceptron = Perceptron(3)
+perceptron.train(training_inp,labels)
+inputs = np.array([1,1,1])
+print(perceptron.predict(inputs))
+    
